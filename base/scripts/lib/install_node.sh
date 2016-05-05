d809615880c8af477f4c31fd9567d9ce820b4a20
skwasha <sascha@umich.edu>
2016-05-05 14:25:42 -0700

    working custom build - had to add back redownload of meteor each time because images were so large

--- base/scripts/lib/install_node.sh
+++ base/scripts/lib/install_node.sh
@@ -11,10 +11,9 @@ NODE_DIST=node-v${NODE_VERSION}-linux-${NODE_ARCH}
 cd /tmp
 curl -O -L http://nodejs.org/dist/v${NODE_VERSION}/${NODE_DIST}.tar.gz
 tar xvzf ${NODE_DIST}.tar.gz
-rm -rf /opt/nodejs || true
+rm -rf /opt/nodejs
 mv ${NODE_DIST} /opt/nodejs
 
 ln -sf /opt/nodejs/bin/node /usr/bin/node
-/opt/nodejs/bin/npm install --prefix /opt/nodejs -g npm@${NPM_VERSION}
+# /opt/nodejs/bin/npm install --prefix /opt/nodejs -g npm@${NPM_VERSION}
 ln -sf /opt/nodejs/bin/npm /usr/bin/npm
-# ln -sf /opt/nodejs/bin/npm /usr/bin/npm
--- base/scripts/lib/install_phantomjs.sh
+++ base/scripts/lib/install_phantomjs.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 # Install PhantomJS
 set -e
 apt-get -y install --no-install-recommends libfreetype6 libfreetype6-dev fontconfig
@@ -6,11 +5,12 @@ ARCH=`uname -m`
 PHANTOMJS_VERSION=2.1.1
 PHANTOMJS_TAR_FILE=phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2
 
-cd /usr/local/share/
-curl -L -O https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2
+cd /usr/local/share
+echo ${PHANTOMJS_TAR_FILE}
+curl -L -O https://bitbucket.org/ariya/phantomjs/downloads/${PHANTOMJS_TAR_FILE}
 tar xjf $PHANTOMJS_TAR_FILE
 ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/local/share/phantomjs
 ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/local/bin/phantomjs
 ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/bin/phantomjs
 
-rm $PHANTOMJS_TAR_FILE || true
+rm $PHANTOMJS_TAR_FILE
