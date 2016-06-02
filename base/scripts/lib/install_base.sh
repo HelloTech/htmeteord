c6747e52837050ac1210cc0dcd0ed9127ae1a880
skwasha <sascha@umich.edu>
2016-06-02 01:10:57 -0700

    base2 - based off node:slim

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 apt-get update -y
-apt-get install -y --no-install-recommends git curl unzip bzip2 ca-certificates cifs-utils sudo procps
+apt-get install -y --no-install-recommends git curl unzip bzip2 ca-certificates cifs-utils sudo procps python build-essential
