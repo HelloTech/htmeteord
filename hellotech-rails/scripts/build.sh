c3c1ea2170f05c568a9795172f583da9d40b4947
Paulo Diniz <pdiniz_100@yahoo.com>
2019-01-10 16:31:57 -0800

    upgrade base

--- hellotech-rails/scripts/build.sh
+++ hellotech-rails/scripts/build.sh
@@ -12,9 +12,9 @@ echo "deb-src http://ftp.us.debian.org/debian/ oldstable main contrib non-free" 
 apt-get -y update
 curl -sL https://deb.nodesource.com/setup_6.x | bash
 apt-get install -y nodejs mysql-client postgresql-client wkhtmltopdf xvfb git libsqlite3-dev nano zlib1g redis-server rabbitmq-server graphviz ghostscript libcdt5 libglapi-mesa libgvc6 librsvg2-common libtiffxx5 libxdot4 libgvpr2 imagemagick libmagickcore-6.q16-2-extra libmagickcore-dev libmagickwand-dev ghostscript gsfonts-x11 netpbm libpaper-utils libpathplan4 ntp oracle-java8-installer oracle-java8-set-default unzip xfonts-base xorg-sgml-doctools discover htop libcroco3 liblzo2-2 libudev1 libpthread-stubs0-dev --no-install-recommends
-wget https://openresty.org/download/openresty-1.11.2.2.tar.gz
-tar -xvzf openresty-1.11.2.2.tar.gz
-cd openresty-1.11.2.2
+wget https://openresty.org/download/openresty-1.13.6.2.tar.gz
+tar -xvzf openresty-1.13.6.2.tar.gz
+cd openresty-1.13.6.2
 ./configure --with-luajit --with-http_gzip_static_module --with-http_iconv_module --with-ipv6 && make && make install
 cd ..
 apt-key adv --fetch-keys http://dl.yarnpkg.com/debian/pubkey.gpg
